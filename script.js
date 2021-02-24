1213a6b096816c3050b2776a9c6aec8d9e0493a0
George_Thomas <George_Thomas@student.mahoningctc.com>
2021-02-24 13:03:15 -0500

    Done

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-//fixed working Timer
+//timer
 var sec = 15;
 function startTimerFunction() {
-    var timer = setInterval(myClock, 1000);
-    //seconds
-    sec = 15;
-    //countdown
-    function myClock() {
-        document.getElementById("countdownClock").innerHTML = --sec;
-        if (sec == 0) {
-            clearInterval(timer);
-      };
+    if (document.getElementById("playerClicks").innerHTML == 0 || document.getElementById("playerClicks").innerHTML == "(Player Score)") {
+        var timer = setInterval(myClock, 1000);
+        //seconds
+        sec = 15;
+        //countdown
+        function myClock() {
+            //counts down
+            document.getElementById("countdownClock").innerHTML = --sec;
+            //stops countdown when timer reaches 0
+            if (sec == 0) {
+                clearInterval(timer);
+            };
+        };
     };
-  };
+};
 
-//Player Click Counter
+//player click counter
 var clickNumber = 0;
 function scoreFunction(){
-    //Prevents counting before timer starts and stops counter a 0 sec
+    //prevents counting before timer starts and stops counter a 0 sec
     if(document.getElementById("countdownClock").innerHTML != 0 && document.getElementById("countdownClock").innerHTML != "--"){
         clickNumber += 1;
     };
     document.getElementById("playerClicks").innerHTML = clickNumber;
     if(sec == 0){
     };
+    //highscore
+    if (document.getElementById("countdownClock").innerHTML == 0 && clickNumber > document.getElementById("highScore").innerHTML){
+        alert("Congrats, you are the highest score. Think you can beat it?");
+    }
+    if (document.getElementById("countdownClock").innerHTML == 0 && clickNumber < document.getElementById("highScore").innerHTML){
+        alert("You are not the highest score. Try again.");
+    }
+    if (clickNumber > document.getElementById("highScore").innerHTML && document.getElementById("countdownClock").innerHTML == 0){
+        document.getElementById("highScore").innerHTML = clickNumber;
+    };
+    //clicks per second
+    document.getElementById("clicksPerSecond").innerHTML = Math.round(clickNumber/(15-document.getElementById("countdownClock").innerHTML));
 };
 
 //reset
 function resetFunction(){
     //stops timer reseting prematurely
-    if(document.getElementById("countdownClock").innerHTML == 0){
+    if(document.getElementById("countdownClock").innerHTML <= 0){
     //timer update
     document.getElementById("countdownClock").innerHTML = "--";
     //click update
@@ -38,9 +54,3 @@ function resetFunction(){
         alert('Please wait for timer to finish before reseting!');
     }
 };
-
-    //highscore (doesnt work)
-    if (highScore > clickNumber){
-        highScore = clickNumber;
-        document.getElementById("highScore").innerHTML = highScore;
-    }
